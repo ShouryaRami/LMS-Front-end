66d982cdab39df898b3ea63f39b86fcfe2481775
ShouryaRami <shouryarami@gmail.com>
2024-02-17 18:36:13 +0530

    qwerty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Admin/Login';
+import Dashboard from './Admin/AdminDashboard';
+import Edit from './Admin/Edit';
+import Add from './Admin/Add';
+import DrawerMenu from './Admin/Drawer';
+import StudentTable from './Admin/Exampls';
+import Dashboard2 from './Admin/Dashboard_test';
+import Login2 from './Admin/LoginOld';
+import CompanyDashboard from './Company/CompanyDashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path='/Admin/Login' element={<Login/>}/>
+            {/* <Route path='/Admin/Login2' element={<Login2/>}/> */}
+            <Route path='/Admin/Dashboard' element={<Dashboard/>}/>
+            {/* <Route path='/Admin/Edit' element={<Edit/>}/>
+            <Route path='/Admin/Add' element={<Add/>}/> */}
+            {/* <Route path='/' element={<StudentTable/>}/> */}
+            {/* <Route path='/Admin/Dashboard2' element={<Dashboard2/>}/> */}
+
+            <Route path='/Company/Dashboard' element={<CompanyDashboard/>}/>
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
