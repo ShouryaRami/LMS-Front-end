64c884d2977b15d546abb00c57026243259e2db5
yagneshacharya <yagneshacharya11@gmail.com>
2024-03-17 13:27:50 +0530

    "Working on Course"

--- src/Company/TestingSkill.js
+++ src/Company/TestingSkill.js
@@ -1,92 +1,79 @@
-// import React, { useState } from 'react';
-// import { FormControl, InputLabel, Select, MenuItem, Chip, makeStyles } from '@mui/material';
-// import { TreeView } from '@mui/x-tree-view';
+import * as React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { TreeView } from "@mui/x-tree-view/TreeView";
+import { TreeItem } from "@mui/x-tree-view/TreeItem";
+import { Button, TextField } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
+export default function FileSystemNavigator() {
+    const [skill, setSkill] = React.useState({});
+    const [val, setVal] = React.useState("");
+    const handleClick = () => {
+        let obj = skill;
+        obj[val] = {};
+        setSkill(obj);
+        setVal("");
+    };
 
-
-// const useStyles = makeStyles((theme) => ({
-//   formControl: {
-//     margin: theme.spacing(1),
-//     minWidth: 120,
-//     maxWidth: 300,
-//   },
-//   chips: {
-//     display: 'flex',
-//     flexWrap: 'wrap',
-//   },
-//   chip: {
-//     margin: 2,
-//   },
-// }));
-
-// const skillsData = [
-//   {
-//     id: 1,
-//     name: 'Programming',
-//     children: [
-//       { id: 11, name: 'JavaScript' },
-//       { id: 12, name: 'Python' },
-//       { id: 13, name: 'Java' },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     name: 'Design',
-//     children: [
-//       { id: 21, name: 'UI Design' },
-//       { id: 22, name: 'UX Design' },
-//     ],
-//   },
-// ];
-
-// const Testing = () => {
-//   const classes = useStyles();
-//   const [selectedSkills, setSelectedSkills] = useState([]);
-
-//   const handleSkillChange = (event) => {
-//     setSelectedSkills(event.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <FormControl className={classes.formControl}>
-//         <InputLabel id="skill-select-label">Select Skills</InputLabel>
-//         <Select
-//           labelId="skill-select-label"
-//           id="skill-select"
-//           multiple
-//           value={selectedSkills}
-//           onChange={handleSkillChange}
-//           renderValue={(selected) => (
-//             <div className={classes.chips}>
-//               {selected.map((value) => (
-//                 <Chip key={value} label={value} className={classes.chip} />
-//               ))}
-//             </div>
-//           )}
-//         >
-//           {skillsData.map((skill) => (
-//             <MenuItem key={skill.id} value={skill.name}>
-//               {skill.name}
-//             </MenuItem>
-//           ))}
-//         </Select>
-//       </FormControl>
-//       {selectedSkills.length > 0 && (
-//         <TreeView>
-//           {selectedSkills.map((selectedSkill) => (
-//             <TreeItem key={selectedSkill} nodeId={selectedSkill} label={selectedSkill}>
-//               {skillsData
-//                 .find((skill) => skill.name === selectedSkill)
-//                 .children.map((subSkill) => (
-//                   <TreeItem key={subSkill.id} nodeId={subSkill.id.toString()} label={subSkill.name} />
-//                 ))}
-//             </TreeItem>
-//           ))}
-//         </TreeView>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Testing;
+    const [subval, setSubVal] = React.useState("");
+    const handleSubClick = (val) => {
+        let obj = skill;
+        obj[val][subval]={}
+        setSkill(obj)
+        setSubVal('')
+    };
+    return (
+        <div>
+            <TextField
+                label={"Tasks"}
+                value={val}
+                onChange={(e) => {
+                    setVal(e.target.value);
+                }}
+            ></TextField>
+            <Button onClick={handleClick}>
+                <AddIcon />
+            </Button>
+            <TreeView
+                aria-label="file system navigator"
+                defaultCollapseIcon={<ExpandMoreIcon />}
+                defaultExpandIcon={<ChevronRightIcon />}
+            >
+                {Object.keys(skill).map((val, idx) => {
+                    return (
+                        <div>
+                            {console.log("Skill -",skill)}
+                            <TreeItem
+                                aria-label="file system navigator"
+                                nodeId={idx}
+                                label={val}
+                            >
+                                <TextField
+                                    label={"Sub Tasks"}
+                                    value={subval}
+                                    onChange={(x) => {
+                                        setSubVal(x.target.value);
+                                    }}
+                                ></TextField>
+                                <Button onClick={()=>handleSubClick(val)}>
+                                    <AddIcon />
+                                </Button>
+                                
+                                {Object.keys(skill[val]).length>=0 && Object.keys(skill[val]).map((subval, subidx) => {
+                                    return (
+                                        <div>
+                                            <TreeItem label={val[subval]} nodeId={subidx} />
+                                            {console.log("skill - ",skill)}
+                                        </div>
+                                    );
+                                })}
+                                {/* <TreeItem label="subval"/> */}
+                            </TreeItem>
+                        </div>
+                    );
+                })}
+            </TreeView>
+        </div>
+    );
+}
